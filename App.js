a2ef4f31a0e47a3148a8bbdf399451438d350be4
jefferson.souza <jefferson.souza@synchro.com.br>
2018-11-27 13:27:30 -0200

    adding styles to the app using stylesheets

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Text, View, Image, Dimensions, FlatList } from 'react-native';
+import { Text, View, Image, Dimensions, FlatList, StyleSheet } from 'react-native';
 
 export default class App extends React.Component {
   render() {
-    const width = Dimensions.get('screen').width;
-
     const data = [
       { id: 1, user: 'john.armless' },
       { id: 2, user: 'mary.christmas' },
@@ -14,21 +12,17 @@ export default class App extends React.Component {
 
     return (
       <>
-        {/* <ScrollView>
-          {data.map(item => (
-            <View key={item.id}>
-              <Text>{item.user}</Text>
-              <Image source={require(`./resources/images/img1.png`)} style={{ width, height: width }} />
-            </View>
-          ))}
-        </ScrollView> */}
         <FlatList
+          style={styles.container}
           data={data}
           keyExtractor={item => `${item.id}`}
           renderItem={item => (
             <View key={item.id}>
-              <Text>{item.user}</Text>
-              <Image source={require(`./resources/images/img1.png`)} style={{ width, height: width }} />
+              <View style={styles.header}>
+                <Image source={require(`./resources/images/img3.jpg`)} style={styles.profileImage} />
+                <Text style={styles.userName}>{item.item.user}</Text>
+              </View>
+              <Image source={require(`./resources/images/img1.png`)} style={styles.post} />
             </View>
           )}
         />
@@ -36,3 +30,29 @@ export default class App extends React.Component {
     );
   }
 }
+
+const width = Dimensions.get('screen').width;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#ddd',
+  },
+  profileImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 10,
+  },
+  userName: {
+    fontWeight: 'bold',
+  },
+  post: {
+    width: width,
+    height: width,
+  },
+});
